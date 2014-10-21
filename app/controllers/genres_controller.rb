1669fb8c38e8dce299f915db44eda4ce11ac5b0f
kararide <karol.karol.92@gmail.com>
2014-10-21 22:08:53 +0200

    add anime form

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,4 +4,29 @@ class GenresController < ApplicationController
 		@genre = Genre.new
 	end
 
+	def index
+		@genres = Genre.all
+	end
+
+	def show
+		@genre = Genre.find(params[:id])
+	end
+
+	def create
+		@genre = Genre.new(secure_params)
+
+		if @genre.valid?
+      flash[:success] = "Genre added!"
+      redirect_to genres_path
+    else
+      render 'new'
+    end
+	end
+
+	private
+
+	def secure_params
+		params.require(:genre).permit(:name)
+	end
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,4 @@
 class Genre < ActiveRecord::Base
-	has_and_belongs_to_many :animes
+	has_and_belongs_to_many :anime
+  validates_uniqueness_of :name
 end
