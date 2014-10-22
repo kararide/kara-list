9544561393db1227fa9c2ef6114e40eb01417300
kararide <karol.karol.92@gmail.com>
2014-10-22 11:48:43 +0200

    fixed genre and anime forms

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -1,2 +1,27 @@
 class AnimesController < ApplicationController
+
+	def index
+		@anime = Anime.all
+	end
+
+	def new
+		@anime = Anime.new
+	end
+
+	def create
+		@anime = Anime.new(animes_params)
+		@anime.save
+		redirect_to @anime
+	end
+
+	def show
+		@anime = Anime.find(params[:id])
+	end
+
+	private 
+
+	def animes_params
+		params.require(:anime).permit([:title, :summary, :episodes, :status, :genre_id, :genre])
+	end
+
 end
--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,3 +1,4 @@
 class Anime < ActiveRecord::Base
   has_and_belongs_to_many :genre
+  validates_uniqueness_of :title
 end
